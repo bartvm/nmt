28e1d9e4c1f21058bab1111a2e5374ea4e803c3f
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2016-02-04 21:33:06 -0500

    Run flake8 on Travis; fix valid -> valid_stream

--- nmt.py
+++ nmt.py
@@ -735,7 +735,7 @@ def train(dim_word_src=100,  # source word vector dimensionality
             if numpy.mod(uidx, validFreq) == 0:
                 use_noise.set_value(0.)
                 valid_errs = pred_probs(f_log_probs, prepare_data,
-                                        model_options, valid)
+                                        model_options, valid_stream)
                 valid_err = valid_errs.mean()
                 history_errs.append(valid_err)
 
@@ -771,7 +771,7 @@ def train(dim_word_src=100,  # source word vector dimensionality
 
     use_noise.set_value(0.)
     valid_err = pred_probs(f_log_probs, prepare_data, model_options,
-                           valid).mean()
+                           valid_stream).mean()
 
     print('Valid ', valid_err)
 
